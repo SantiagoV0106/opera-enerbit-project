72213fde7a28f77ef25680b95d7831c1bd8355e5
SantiagoV0106 <88668447+SantiagoV0106@users.noreply.github.com>
2023-11-28 17:09:12 -0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://SantiagoV0106.github.io/opera-enerbit-project"
 })
